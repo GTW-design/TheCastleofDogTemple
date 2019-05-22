f391d71f5b3c1b86e34b13a6d3abcd07d0a15054
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2019-05-22 16:28:49 +1000

    Start of CameraStuff.cs

---
+++ TheCastle0fDogTemple/Assets/Scripts/CameraStuff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStuff : MonoBehaviour {
+
+	public Transform anchorPoint;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
